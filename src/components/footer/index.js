c33477c25beaeb9d65949a38e6119435beaba1c7
gyanish77 <gyanish@luminoguru.com>
2022-05-03 11:14:36 +0530

    Commit

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./footer.css";
 function Footer() {
-  return <div className="footer">Made with 🔥 by Gyanish Shrivastava</div>;
+  return <div className="footer">Made with 🧡 by Gyanish Shrivastava</div>;
 }
 
 export default Footer;
